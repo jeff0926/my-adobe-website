7348a812a33e92bd2a2d169d65615af5578836c0
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-02 12:16:17 -0400

    Update scripts.js removed importExternal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -119,17 +119,7 @@ function setupClickstreamCapture() {
   alert('setupClickstreamCapture()');
   const clickstream = [];
   
-//JC : import SAP UI5 <webcomponents>  
-// Define an async function to import the external module
-async function importExternalModule() {
-  try {
-    const module = await import('https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js');
-    alert('You can now use the module and its functionality');
-    console.log('External module has been imported successfully');
-  } catch (error) {
-    console.error('Failed to import the external module:', error);
-  }
-}
+
 
 // Call the async function to initiate the import
 //importExternalModule();
