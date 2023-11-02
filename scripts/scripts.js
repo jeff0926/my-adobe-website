d6044714251d283cdbb4a170e68dfcbfaf5afd53
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-02 11:48:56 -0400

    Update scripts.js added alert comment to importExternal()

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -123,7 +123,7 @@ function setupClickstreamCapture() {
 async function importExternalModule() {
   try {
     const module = await import('https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js');
-    // You can now use the module and its functionality
+    alert('You can now use the module and its functionality')
     console.log('External module has been imported successfully');
   } catch (error) {
     console.error('Failed to import the external module:', error);
