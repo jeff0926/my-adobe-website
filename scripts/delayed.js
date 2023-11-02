473c04dabd3aa2decc3d7e320ab43c86a4e1ceb5
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-02 12:16:47 -0400

    Update delayed.js added impoertExternal

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,5 +3,15 @@ import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-importExternalModule();
+//JC : import SAP UI5 <webcomponents>  
+// Define an async function to import the external module
+async function importExternalModule() {
+  try {
+    const module = await import('https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js');
+    alert('You can now use the module and its functionality');
+    console.log('External module has been imported successfully');
+  } catch (error) {
+    console.error('Failed to import the external module:', error);
+  }
+}
 // add more delayed functionality here
