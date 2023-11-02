1a7bfd3ed8c2c509bb9e021edfe2200e58a8a921
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-02 12:58:55 -0400

    Update delayed.js added an ui5 card html block
    Update delayed.js added an ui5 card html block

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,12 +5,30 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 //after ui5 library is ready inject ui5 button as a test
 function create_ui5() {
+
+  var htmlString = `
+<ui5-card class="tile">
+  <ui5-card-header slot="header" title-text="UI5-card with ui5-list + ui5-li" subtitle-text="Below ui5-list ui5-li" status="3 of 10">
+    <ui5-icon name="group" slot="avatar"></ui5-icon>
+    <ui5-button design="Transparent" slot="action" onclick="showAlert('Tile 6 UI5 CARD')">View All</ui5-button>
+  </ui5-card-header>
+  <div class="card-content">
+    <ui5-list separators="None" class="card-content-child" style="width: 100%; margin-bottom: 0.75rem;">
+      <ui5-li image="https://www.w3schools.com/howto/img_avatar2.png" description="User Researcher">Alain Chevalier</ui5-li>
+      <ui5-li image="https://www.w3schools.com/howto/img_avatar.png" description="Artist">Monique Legrand</ui5-li>
+      <ui5-li image="https://www.w3schools.com/howto/img_avatar2.png" description="UX Specialist">John Smith</ui5-li>
+      <ui5-li image="https://www.w3schools.com/howto/img_avatar.png" description="UX Specialist">Isabella Adams</ui5-li>
+    </ui5-list>
+  </div>
+</ui5-card>`;
+
+  
   // Find the element by its class (assuming the class is "cards ui5-button")
   console.log('creating ui5-button into div cards.ui5-button ');
   var targetDiv = document.querySelector('div.cards.ui5-button');
 
   // Define the new HTML content
-  var newContent = '<div class="cards ui5-button"><ui5-button design="Transparent">View All</ui5-button></div>';
+  var newContent = htmlString;
 
   // Add the new HTML content to the existing content
   if (targetDiv) {
