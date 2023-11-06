2bc3a79123746ab47113d2f4d2af607220584553
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-05 20:11:44 -0500

    Update scripts.js added clickStream alert details

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -116,7 +116,7 @@ async function loadLazy(doc) {
   sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
 function setupClickstreamCapture() {
-  alert('setupClickstreamCapture()');
+  alert('This is an alert JC custom clcikstream events are setupClickstreamCapture() look @ console for events');
   const clickstream = [];
   
 
