762cfc135117812d7d3a66cab94246719e362fcc
jeff0926 <119612303+jeff0926@users.noreply.github.com>
2023-11-27 19:05:59 -0500

    Update delayed.js used https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -47,7 +47,7 @@ alert("called create_ui5()");
 async function importExternalModule() {
   alert('importExternalModule');
   try {
-    const module = await import('https://ui5.sap.com/resources/sap-ui-core.js');
+    const module = await import('https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js');
     //https://ui5.sap.com/resources/sap-ui-core.js
     //https://sap.github.io/ui5-webcomponents/assets/js/ui5-webcomponents/bundle.esm.js
     //alert('Jeff C : UI5 webcomponent library initiated and available: You can now use the UI5 library and its functionality');
