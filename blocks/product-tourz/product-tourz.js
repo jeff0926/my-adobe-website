f84d5557efc5f4557d9a4eea1dadb69d6e00ac49
Jeff <119612303+jeff0926@users.noreply.github.com>
2024-04-23 09:21:41 -0400

    Add files via upload

--- blocks/product-tourz/product-tourz.js
+++ blocks/product-tourz/product-tourz.js
@@ -1,25 +1,14 @@
 export default function decorate(block) {
     console.log('Decorating block...');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('enter');
-                console.log('Element entered:', entry.target);
-            }
-        });
-    });
-
-    const sections = block.querySelectorAll(':scope > div');
-    console.log('Found sections:', sections.length);
-
+    const sections = block.querySelectorAll('.oli-tour > div');
     let currentMainSection = '';
 
     sections.forEach((section, index) => {
         const sectionType = section.querySelector('div:first-child');
         if (sectionType !== null) {
             currentMainSection = section.querySelector('div:nth-child(2)').textContent.trim();
-            console.log(`Main section: ${currentMainSection}`);
+            console.log(`${currentMainSection}`);
         } else {
             const description = section.querySelector('div:nth-child(2)').textContent.trim();
             console.log(`  Child of ${currentMainSection}: Step ${index + 1}:`);
@@ -30,9 +19,6 @@ export default function decorate(block) {
 
             const imageAlt = section.querySelector('div:nth-child(4) picture img').getAttribute('alt');
             console.log(`    Image Alt Text: ${imageAlt}`);
-
-            observer.observe(section);
-            console.log('Observed section:', section);
         }
     });
 
