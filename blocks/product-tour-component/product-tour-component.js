9a3cef9e28c38e33daea56677e2fd90b2e728372
Jeff <119612303+jeff0926@users.noreply.github.com>
2024-04-24 18:18:20 -0400

    Add files via upload

--- blocks/product-tour-component/product-tour-component.js
+++ blocks/product-tour-component/product-tour-component.js
@@ -1,4 +1,7 @@
-// Create the product-tour-component element with data-url attribute
+// Find the product-tour-component div
+const productTourComponentDiv = document.querySelector('.product-tour-component');
+
+// Create the product-tour-component element
 const productTourComponent = document.createElement('product-tour-component');
 productTourComponent.setAttribute('data-url', 'https://raw.githubusercontent.com/jeff0926/cdn-deliver/main/integrated-business-planning.json');
 
@@ -7,6 +10,6 @@ const scriptElement = document.createElement('script');
 scriptElement.src = 'https://main--my-adobe-website--jeff0926.hlx.page/blocks/product-tour-component/product-tour-component-build.js';
 scriptElement.defer = true;
 
-// Append elements to the document body
-document.body.appendChild(productTourComponent);
-document.body.appendChild(scriptElement);
+// Insert product-tour-component and scriptElement after the product-tour-component div
+productTourComponentDiv.parentNode.insertBefore(productTourComponent, productTourComponentDiv.nextSibling);
+productTourComponentDiv.parentNode.insertBefore(scriptElement, productTourComponentDiv.nextSibling);
